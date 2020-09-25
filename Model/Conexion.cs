21173bceb254d3a079de8b04f61c81b804465bff
sebastiangarzon2001 <71908453+sebastiangarzon2001@users.noreply.github.com>
2020-09-25 18:51:18 -0500

    Add files via upload

---
+++ Model/Conexion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace colegio2020media.Model
+{
+    class Conexion
+    {
+        SqlConnection conn;
+
+        public void Conector() 
+        {
+            conn = new SqlConnection(@"Data Source=FAMILIAGARCAR\SQLEXPRESS;Initial Catalog=College;Integrated Security=True");
+            conn.Open();
+        }
+
+        public void Desconectar() 
+        {
+            conn.Close();
+        }
+
+        public void EjecutarSQL(String consulta) 
+        {
+            SqlCommand com = new SqlCommand(consulta, conn);
+
+            int filasAfectadas = com.ExecuteNonQuery();
+
+            if (filasAfectadas > 0)
+            {
+                MessageBox.Show("Operacion realizada con exito",
+                                "La base de datos ha sido actualizada", MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+            else 
+            {
+                MessageBox.Show("No se ha modificada la base de datos",
+                                "error del sistema",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        public void ActualizarGrid(DataGridView dg, String consulta)
+        {
+            this.Conector();
+
+            System.Data.DataSet ds = new System.Data.DataSet();
+
+            SqlDataAdapter da = new SqlDataAdapter(consulta, conn);
+
+            da.Fill(ds, "PersonaAdministrador");
+            dg.DataMember = "PersonaAdministrador";
+
+            this.Desconectar();
+        }
+
+
+
+
+    }
+}
